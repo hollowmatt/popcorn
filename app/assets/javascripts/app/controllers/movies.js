2d501ab7e23fa4cd245822ebf1ad529ceae9a602
Matthew Holloway <matthew.holloway@logicwerx.com>
2014-05-14 22:00:26 -0700

    Added actual YouTube data with API calls, promises

--- app/assets/javascripts/app/controllers/movies.js
+++ app/assets/javascripts/app/controllers/movies.js
@@ -2,9 +2,10 @@ angular.module('popcornApp.controllers')
 	.controller('MoviesController',
 	function($scope, MoviesService) {
 		console.log("MoviesController is booting");
-		
-    $scope.movies = MoviesService.movies();
-    
+	
+    MoviesService.movies().then(function(movies) {
+       $scope.movies = movies;  
+    })
    	$scope.addFavorite = function(movie) {
    		movie.isFavorite = true;
    	};
