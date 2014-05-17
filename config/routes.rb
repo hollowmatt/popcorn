c84eaaebadf5a6ee3f8bd06448d21e0e028c54fd
Matthew Holloway <matthew.holloway@logicwerx.com>
2014-05-16 22:05:39 -0700

    Added movies controller and methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Popcorn::Application.routes.draw do
   devise_for :users
+  resources :movies
   get "popcorn/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
