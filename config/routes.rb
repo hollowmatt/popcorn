c3cf2b85b0df22654a492d66ec01827bbbe79fa1
Matthew Holloway <matthew.holloway@logicwerx.com>
2014-05-16 21:40:25 -0700

    added devise and some models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Popcorn::Application.routes.draw do
+  devise_for :users
   get "popcorn/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
