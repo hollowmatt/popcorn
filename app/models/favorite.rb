78413232302b34e18e2de81aa33bc58629c2261c
Matthew Holloway <matthew.holloway@logicwerx.com>
2014-05-17 14:51:29 -0700

    Added favorites controller, routes

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,7 @@
 class Favorite < ActiveRecord::Base
   belongs_to :movie
   belongs_to :user
+
+  validates_uniqueness_of :user_id, :scope => :movie_id
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Popcorn::Application.routes.draw do
   devise_for :users
   resources :movies
-  get "popcorn/index"
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  resources :favorites
 
-  # You can have the root of your site routed with "root"
+  get "popcorn/index"
   root 'popcorn#index'
 
   # Example of regular route:
