dd6a8756db1ff112cb574a98a2d1f4c557093f59
Matthew Holloway <matthew.holloway@logicwerx.com>
2014-05-18 20:33:23 -0700

    Resource: movies (ch 11, 22:02)

--- app/assets/javascripts/app/resources.js
+++ app/assets/javascripts/app/resources.js
@@ -1,8 +1,32 @@
 angular.module('popcornApp.resources', ['rails'])
 .factory("Movie",
-	function(railsResourceFactory) {
-		return railsResourceFactory({
+	function(railsResourceFactory, $q) {
+		var resource = railsResourceFactory({
 			url: '/movies',
 			name: 'movie'
 		});
+
+		resource.findOrCreateByYoutubeId = function (youtubeId, otherAttributes) {
+			var d = $q.defer();
+			resource.query({youtube_id: youtubeId}).
+				then(function(movies) {
+					if(movies.length > 0) {
+						//return
+						// console.log(movies[0]);
+						d.resolve(movies[0]);
+					} else {
+						//create
+						var createAttributes = _.extend(
+							otherAttributes, {youtube_id: youtubeId}
+						);
+						var movie = new resource(createAttributes);
+						movie.save().then(function() {
+							// console.log(movie);
+							d.resolve(movie);	
+						});
+					}
+				});
+				return d.promise;
+		};
+		return resource;
 	});
