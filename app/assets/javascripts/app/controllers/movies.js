726d3bc8b38caab80e50c15441958b36e333e8c5
Matthew Holloway <matthew.holloway@logicwerx.com>
2014-05-24 18:23:14 -0700

    Load movies from different lists, add cache to lists

--- app/assets/javascripts/app/controllers/movies.js
+++ app/assets/javascripts/app/controllers/movies.js
@@ -1,9 +1,13 @@
 angular.module('popcornApp.controllers')
 	.controller('MoviesController',
 	function($scope, MoviesService, Favorite, UserService, $q) {
-		console.log("MoviesController is booting");
-	
-    MoviesService.movies().then(function(movies) {
-      $scope.movies = movies;
-    });
+		
+		$scope.$watch('chart', function(newValue) {
+			if(newValue) {
+				MoviesService.movies(newValue).then(function(movies) {
+					$scope.movies = movies;
+				});
+			}
+		});
+    $scope.chart = "most_popular";
 	});
--- app/assets/javascripts/app/services/movies.js
+++ app/assets/javascripts/app/services/movies.js
@@ -1,36 +1,55 @@
 angular.module('popcornApp.services')
-	.service('MoviesService', function($http, $q, Movie) {
-		this.movies = function(name) {
+	.service('MoviesService', function($http, $q, Movie, $cacheFactory) {
+		var moviesCache = $cacheFactory('movies');
+
+    this.movies = function(chart) {
+      chart = typeof chart !== 'undefined' ? chart : "most_popular";
       var d = $q.defer();
-		  $http({
-        method: 'GET',
-        url: 'http://gdata.youtube.com/feeds/api/charts/movies/most_popular?v=2&max-results=12&paid-content=true&hl=en&region=us&alt=json'
-      }).	
-			then(function(response) {
-        var movies = _.map(response.data.feed.entry, function(movie){
-          return {
-            youtubeId: movie['media$group']['yt$videoid']['$t'],
-            title: movie['media$group']['media$title']['$t'], 
-            released: movie['yt$firstReleased']['$t'].match(/\d{4}/)[0],
-            rated: movie['media$group']['media$rating'][0]['$t'],
-            runningTime: Math.round(movie['media$group']['yt$duration']['seconds'] / 60),
-            posterUrl: _.findWhere(movie['media$group']['media$thumbnail'], {"yt$name": "poster"}).url,
-            description: movie['media$group']['media$description']['$t']
-          };
-        })
+		    
+      var getNested = function(data, path) {
+        var i, len = path.length;
+        for(i = 0; typeof data === 'object' && i < len; ++i) {
+          data = data[path[i]];
+        }
+        return data;
+      };
 
-        var moviePromises = _.map(movies, function(movieData) {
-          var youtubeId = movieData.youtubeId;
-          return Movie.findOrCreateByYoutubeId(youtubeId, movieData);
-        });
+      var cachedMovies = moviesCache.get(chart);
 
-        $q.all(moviePromises).then(function(movieResources) {
-          d.resolve(movieResources);
+      if(cachedMovies) {
+        d.resolve(cachedMovies);
+      } else {
+        $http({
+          method: 'GET',
+          url: 'http://gdata.youtube.com/feeds/api/charts/movies/' + chart 
+          +'?v=2&max-results=15&paid-content=true&hl=en&region=us&alt=json'
+        }). 
+        then(function(response) {
+          var movies = _.map(response.data.feed.entry, function(movie){
+            return {
+              youtubeId: movie['media$group']['yt$videoid']['$t'],
+              title: movie['media$group']['media$title']['$t'], 
+              released: movie['yt$firstReleased']['$t'].match(/\d{4}/)[0],
+              rated: getNested(movie, ['media$group','media$rating',0,'$t']),
+              runningTime: Math.round(movie['media$group']['yt$duration']['seconds'] / 60),
+              posterUrl: _.findWhere(movie['media$group']['media$thumbnail'], {"yt$name": "poster"}).url,
+              description: movie['media$group']['media$description']['$t']
+            };
+          })
+
+          var moviePromises = _.map(movies, function(movieData) {
+            var youtubeId = movieData.youtubeId;
+            return Movie.findOrCreateByYoutubeId(youtubeId, movieData);
+          });
+
+          $q.all(moviePromises).then(function(movieResources) {
+            d.resolve(movieResources);
+          });
+        },
+        function(error) {
+          d.reject(error);
         });
-      },
-      function(error) {
-        d.reject(error);
-      });
-      return d.promise;
-		}
+        return d.promise;
+      }  
+    }
 	});
