3cd958a0c1adc85bfc042104d6c5701073d4609f
Matt Holloway <mholloway@book.com>
2014-06-01 11:26:30 -0700

    Have the user profile working (up to Ch 17.5)

--- app/assets/javascripts/app/interceptors/user_auth.js
+++ app/assets/javascripts/app/interceptors/user_auth.js
@@ -1,10 +1,20 @@
 angular.module('popcornApp.interceptors')
-	.factory('UserAuthInterceptor',
+  .factory('UserAuthInterceptor',
      function($rootScope, $q, AuthService) {
        return {
          'request': function(req) {
-           console.log("making a request");
-           return req;
+           var d = $q.defer();
+           AuthService.currentUser().then(function(user) {
+             if(user) {
+               req.params = req.params || {};
+               req.params['auth_token'] = req.params['auth_token'] || user.auth_token;
+               req.params['auth_user_id'] = req.params['auth_user_id'] || user.id;
+               d.resolve(req);
+             } else {
+               d.resolve(req);
+             }
+           });
+           return d.promise;
          },
          'requestError': function(reqErr) { 
            return reqErr;
